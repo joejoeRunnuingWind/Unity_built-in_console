a28691bf4555eb478e1bd55d3519e1ffc3675a87
JSH\shihao <shihaojiao@gmail.com>
2016-08-19 18:16:31 +0200

    Script change button sprites

---
+++ GUIElementHandle.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class GUIElementHandle : MonoBehaviour {
+
+	GameObject previousClicked;
+	Sprite imageN;
+	Sprite imageG;
+
+	// Use this for initialization
+	void Start () {
+		imageN = (Sprite)Resources.Load("Images/GUI/ButtonNoir", typeof(Sprite));
+		imageG = (Sprite)Resources.Load("Images/GUI/ButtonGris", typeof(Sprite));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (EventSystem.current.currentSelectedGameObject != null) {
+			if (previousClicked != null) {
+				previousClicked.GetComponent<Image> ().overrideSprite = imageG;
+			}
+			//Debug.Log (EventSystem.current.currentSelectedGameObject);
+			GameObject currentOnClick = EventSystem.current.currentSelectedGameObject;
+			if (currentOnClick.tag == "alertButton") {
+				//Debug.Log ("change color");
+				//Sprite imageN = (Sprite)Resources.Load("Images/GUI/ButtonNoir", typeof(Sprite));
+				//Texture2D imageB = (Texture2D)Resources.Load("Images/GUI/ButtonNoir", typeof(Texture2D));
+				Debug.Log (imageN);
+				currentOnClick.GetComponent<Image> ().overrideSprite = imageN;
+				previousClicked = currentOnClick;
+			}
+		}
+	}
+}
